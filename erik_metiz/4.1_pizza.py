99d7b3a3125c6d8c2db4ccec3401a77790fdb53b
Maryamka <145837736+StarMarpp@users.noreply.github.com>
2024-01-27 19:55:30 +0300

    Update 4.1_pizza.py

---
+++ erik_metiz/4.1_pizza.py
@@ -0,0 +1,4 @@
+favorite_pizza = ['сезонный', 'маргарита', 'классический']
+for pizza in favorite_pizza:
+    print(f"Я давно не пробовала, {pizza.lower()}." )
+    print(f"Вседа ее заказываю, когда прихожу в кафе.\n")
